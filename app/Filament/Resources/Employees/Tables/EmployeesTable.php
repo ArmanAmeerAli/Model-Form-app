183756934f8e5f25c08f1a8b8e708d3d7e828c42
ArmanAmeerAli <154812221+ArmanAmeerAli@users.noreply.github.com>
2025-10-21 17:21:14 +0500

    Added a Delete Action for Individual Employee

--- app/Filament/Resources/Employees/Tables/EmployeesTable.php
+++ app/Filament/Resources/Employees/Tables/EmployeesTable.php
@@ -4,6 +4,7 @@ namespace App\Filament\Resources\Employees\Tables;
 
 use Filament\Actions\BulkActionGroup;
 use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\DeleteAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
@@ -26,6 +27,7 @@ class EmployeesTable
             ])
             ->recordActions([
                 EditAction::make(),
+                DeleteAction::make(),
             ])
             ->toolbarActions([
                 BulkActionGroup::make([
