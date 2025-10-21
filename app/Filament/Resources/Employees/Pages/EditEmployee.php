10e69ce7b07a0940656c9448bf90df425014ce65
ArmanAmeerAli <154812221+ArmanAmeerAli@users.noreply.github.com>
2025-10-21 17:41:48 +0500

    Adding a Back button when, user comes to view and edit an Employee

--- app/Filament/Resources/Employees/Pages/EditEmployee.php
+++ app/Filament/Resources/Employees/Pages/EditEmployee.php
@@ -3,7 +3,9 @@
 namespace App\Filament\Resources\Employees\Pages;
 
 use App\Filament\Resources\Employees\EmployeeResource;
+use Filament\Actions\Action;
 use Filament\Actions\DeleteAction;
+use Filament\Actions\ViewAction;
 use Filament\Resources\Pages\EditRecord;
 
 class EditEmployee extends EditRecord
@@ -17,6 +19,12 @@ class EditEmployee extends EditRecord
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('back')
+            ->label('Back')
+            ->url(EmployeeResource::getUrl('index'))
+            ->color('success')
+            ->icon('heroicon-o-arrow-left'),
+            ViewAction::make(),
             DeleteAction::make(),
         ];
     }
