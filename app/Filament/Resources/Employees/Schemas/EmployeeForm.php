d65e4070aeb7340e06fd7dd9bb6b94ae6cdbc1d0
ArmanAmeerAli <154812221+ArmanAmeerAli@users.noreply.github.com>
2025-10-21 17:59:40 +0500

    Adding Validations to the Form

--- app/Filament/Resources/Employees/Schemas/EmployeeForm.php
+++ app/Filament/Resources/Employees/Schemas/EmployeeForm.php
@@ -2,6 +2,7 @@
 
 namespace App\Filament\Resources\Employees\Schemas;
 
+use App\Models\Employee;
 use Filament\Forms\Components\TextInput;
 use Filament\Schemas\Schema;
 
@@ -11,12 +12,44 @@ class EmployeeForm
     {
         return $schema
             ->components([
-                TextInput::make('first_name'),
-                TextInput::make('last_name'),
-                TextInput::make('email'),
-                TextInput::make('phone'),
-                TextInput::make('position'),
-                TextInput::make('salary'),
+                TextInput::make('first_name')
+                    ->label('First Name')
+                    ->required()
+                    ->minLength(2)
+                    ->maxLength(50),
+                TextInput::make('last_name')
+                    ->label('Last Name')
+                    ->required()
+                    ->minLength(2)
+                    ->maxLength(50),
+                TextInput::make('email')
+                    ->label('Email')
+                    ->required()
+                    ->unique(Employee::class, 'email', ignoreRecord: true) //current record is excluded when checking for uniqueness
+                    ->maxLength(150)
+                    ->email(),
+                TextInput::make('phone')
+                    ->label('Phone Number')
+                    ->required()
+                    ->tel()
+                    ->unique(Employee::class, 'phone', ignoreRecord: true)
+                    ->rules(['required', 'digits:10', 'numeric']) // phone number must be 11 digits and numeric
+                    ->helpertext('Enter a 10-Digit Phone number(numbers only, no spaces, no special characters)')
+                    ->validationAttribute('Phone Number'), //Validation attribute method to change the default validation for the phone field
+                TextInput::make('position')
+                    ->label('Job Title')
+                    ->required()
+                    ->minLength(4)
+                    ->maxLength(150),
+                TextInput::make('salary')
+                    ->label('Salary')
+                    ->required()
+                    ->numeric() //This enforce the input can only be a numerical value
+                    ->minValue(1000)
+                    ->maxValue(99999999.99)
+                    ->step(0.01) //input allow upto 2 decimal places
+                    ->placeholder('5000.00')
+                    ->helperText('Enter a Salary between 1000.00 and 99999999.99')
             ]);
     }
 }
