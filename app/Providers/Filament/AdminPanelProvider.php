736e5bc980dff74963e5c3ecd23957be6abfe944
ArmanAmeerAli <154812221+ArmanAmeerAli@users.noreply.github.com>
2025-10-21 17:21:45 +0500

    Changed the color of theme

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -38,7 +38,7 @@ class AdminPanelProvider extends PanelProvider
             ->profile()
             ->brandName('Arman')
             ->colors([
-                'primary' => Color::Rose,
+                'primary' => Color::Yellow,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\Filament\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\Filament\Pages')
